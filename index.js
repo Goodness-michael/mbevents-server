124116b1559cb58a02b84d340f61517a066539d1
cruizeartmedia@gmail.com <cruizeartmedia@gmail.com>
2024-12-09 13:10:20 +0100

    fixed bug

--- index.js
+++ index.js
@@ -3,23 +3,23 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const mongoose = require("mongoose");
-const cors = require('cors')
-const fileupload = require('express-fileupload')
-const cloudinary = require('cloudinary').v2
+const cors = require("cors");
+const fileupload = require("express-fileupload");
+const cloudinary = require("cloudinary").v2;
 const userRouter = require("./routes/userRouter");
-const eventRouter = require('./routes/eventRouter')
+const eventRouter = require("./routes/eventRouter");
 
 //cloudinary config
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET, 
+  api_secret: process.env.API_SECRET,
 });
 
 // middleware
-app.use(fileupload({useTempFiles: true})) //req.files
+app.use(fileupload({ useTempFiles: true })); //req.files
 app.use(express.json()); //req.body
-app.use(cors()) // 
+app.use(cors()); //
 
 // Routes
 
@@ -27,7 +27,7 @@ app.get("/", (req, res) => {
   res.status(200).json({ success: true, message: "Mb Events Server" });
 });
 app.use("/api/v1", userRouter);
-app.use('/api/v1/events', eventRouter)
+app.use("/api/v1/events", eventRouter);
 
 // Error Route
 app.use((req, res) => {
